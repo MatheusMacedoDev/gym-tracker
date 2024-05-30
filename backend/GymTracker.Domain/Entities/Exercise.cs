209f461c91d3de05e11cc16b716106aafb55530c
Gabriela Rosa <gabii.ramos1309@gmail.com>
2024-05-30 17:20:13 -0300

    Exercise entity done

---
+++ backend/GymTracker.Domain/Entities/Exercise.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymTracker.Domain.Entities;
+
+[Table("exercises")]
+[Index(nameof(ExerciseName), IsUnique = true)]
+public class Exercise
+{
+    [Key]
+    [Column("exercise_id")]
+    public Guid ExerciseId { get; private set; }
+
+    [Required]
+    [Column("exercise_name")]
+    public string? ExerciseName { get; private set; }
+
+    [Column("exercise_gif")]
+    public string? ExerciseGif { get; private set; }
+
+    protected Exercise()
+    {
+    }
+
+    public Exercise(string exerciseName, string exerciseGif)
+    {
+        ExerciseId = Guid.NewGuid();
+        ExerciseName = exerciseName;
+        ExerciseGif = exerciseGif;
+    }
+}
