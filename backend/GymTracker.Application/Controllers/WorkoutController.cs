4447331d6fb718787dd6275dd29e935913eb031e
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-31 14:50:43 -0300

    Register default workout feature done

---
+++ backend/GymTracker.Application/Controllers/WorkoutController.cs
@@ -0,0 +1,33 @@
+using GymTracker.Application.Services;
+using GymTracker.Application.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GymTracker.Application.Controllers;
+
+[Route("api/workouts")]
+[ApiController]
+[Produces("application/json")]
+public class WorkoutController : ControllerBase
+{
+    private readonly IDefaultWorkoutService _defaultWorkoutService;
+
+    public WorkoutController(IDefaultWorkoutService defaultWorkoutService)
+    {
+        _defaultWorkoutService = defaultWorkoutService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> RegisterDefaultWorkout([FromBody] RegisterDefaultWorkoutRequest request)
+    {
+        try
+        {
+            var response = await _defaultWorkoutService.RegisterDefaultWorkout(request);
+
+            return StatusCode(201, response);
+        }
+        catch (Exception error)
+        {
+            return BadRequest(error.Message);
+        }
+    }
+}
