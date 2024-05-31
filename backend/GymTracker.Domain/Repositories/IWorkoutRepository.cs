35615abcc6939a4b6823987713d78ef6c815c434
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-31 16:21:21 -0300

    Unnused workout repository method removed

--- backend/GymTracker.Domain/Repositories/IWorkoutRepository.cs
+++ backend/GymTracker.Domain/Repositories/IWorkoutRepository.cs
@@ -5,5 +5,4 @@ namespace GymTracker.Domain.Repositories;
 public interface IWorkoutRepository
 {
     Task CreateDefaultWorkout(DefaultWorkout defaultWorkout);
-    Task<List<DefaultWorkout>> ListDefaultWorkoutsByUserId(Guid userId);
 }
--- backend/GymTracker.Infra/Data/Repositories/WorkoutRepository.cs
+++ backend/GymTracker.Infra/Data/Repositories/WorkoutRepository.cs
@@ -1,7 +1,6 @@
 using GymTracker.Domain.Entities;
 using GymTracker.Domain.Repositories;
 using GymTracker.Infra.Data;
-using Microsoft.EntityFrameworkCore;
 
 namespace GymTracker.Infra.Repositories;
 
@@ -17,12 +16,5 @@ public class WorkoutRepository : IWorkoutRepository
     public async Task CreateDefaultWorkout(DefaultWorkout defaultWorkout)
     {
         await _context.DefaultWorkouts!.AddAsync(defaultWorkout);
-    }
-
-    public async Task<List<DefaultWorkout>> ListDefaultWorkoutsByUserId(Guid userId)
-    {
-        return await _context.DefaultWorkouts!
-            .Where(workout => workout.UserId == userId)
-            .ToListAsync();
     }
 }
