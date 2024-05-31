c4b0629ac827623c6fd5f4ef4fda4f190a19744d
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-05-31 15:02:18 -0300

    List default workouts by specific user added to WorkoutRepository

--- backend/GymTracker.Domain/Repositories/IWorkoutRepository.cs
+++ backend/GymTracker.Domain/Repositories/IWorkoutRepository.cs
@@ -5,4 +5,5 @@ namespace GymTracker.Domain.Repositories;
 public interface IWorkoutRepository
 {
     Task CreateDefaultWorkout(DefaultWorkout defaultWorkout);
+    Task<List<DefaultWorkout>> ListDefaultWorkoutsByUserId(Guid userId);
 }
--- backend/GymTracker.Infra/Data/Repositories/WorkoutRepository.cs
+++ backend/GymTracker.Infra/Data/Repositories/WorkoutRepository.cs
@@ -1,6 +1,7 @@
 using GymTracker.Domain.Entities;
 using GymTracker.Domain.Repositories;
 using GymTracker.Infra.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace GymTracker.Infra.Repositories;
 
@@ -16,5 +17,12 @@ public class WorkoutRepository : IWorkoutRepository
     public async Task CreateDefaultWorkout(DefaultWorkout defaultWorkout)
     {
         await _context.DefaultWorkouts!.AddAsync(defaultWorkout);
+    }
+
+    public async Task<List<DefaultWorkout>> ListDefaultWorkoutsByUserId(Guid userId)
+    {
+        return await _context.DefaultWorkouts!
+            .Where(workout => workout.UserId == userId)
+            .ToListAsync();
     }
 }
