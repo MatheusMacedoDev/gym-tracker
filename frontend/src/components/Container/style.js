4683908cef1f398c0175e10df6d80940b88631ae
Eduardo Pasqualetti <eduardopasqualetti9@gmail.com>
2024-05-30 23:14:26 -0300

    Container component created

---
+++ frontend/src/components/Container/style.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+import { colors } from '../../colors.config';
+
+export const Container = styled.View`
+    flex: 1;
+    align-items: center;
+    padding: 20px;
+    border: black;
+    border-width: 2px;
+    width: 100%;
+    background-color: black;
+`;
