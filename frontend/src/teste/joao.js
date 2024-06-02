95c5c3bc3257a71f0c89a77bb649a9fae274c342
joao <joao.oliveiira.pda@gmail.com>
2024-06-02 17:56:38 -0300

    uploaded package.json  with changes of packages

--- frontend/src/teste/joao.js
+++ frontend/src/teste/joao.js
@@ -1,13 +1,30 @@
-import { Text, View } from "react-native"
-import { CardMan } from "../components/CardGender/indexMan"
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import LineChartComponent from '../components/Grafic/index'; // ajuste o caminho conforme necessário
+
+const data = {
+  labels: ["dom", "seg", "ter", "qua", "qui", "sex"],
+  datasets: [
+    {
+      data: [10, 50, 30, 20, 40, 60], // Exemplo de progressão linear
+      strokeWidth: 2, // optional
+      color: (opacity = 1) => `rgba(255, 152, 0, ${opacity})`, // Laranja para os pontos e linhas
+    }
+  ],
+  legend: ["Progressão ao longo do tempo"]
+};
 
 export const Joao = () => {
-    return(
-        <View>
-            <CardMan title={'Masculino'}>
+  return (
+    <SafeAreaView style={styles.container}>
+      <LineChartComponent data={data} />
+    </SafeAreaView>
+  );
+};
 
-            </CardMan>
-           
-        </View>
-    )
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000', // Mude a cor de fundo conforme necessário
+  },
+});
