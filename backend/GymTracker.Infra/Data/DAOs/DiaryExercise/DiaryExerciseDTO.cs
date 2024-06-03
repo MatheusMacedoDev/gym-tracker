9f38ebe24956023919494f234d865e49d4416788
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-06-03 10:51:24 -0300

    DiaryExerciseDTO created

---
+++ backend/GymTracker.Infra/Data/DAOs/DiaryExercise/DiaryExerciseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace GymTracker.Infra.Data.DAOs.DiaryExercise;
+public record DiaryExerciseDTO
+(
+    Guid diaryExerciseId,
+    string exerciseName,
+    string exerciseGif,
+    string muscleGroups,
+    string repetitionsRange,
+    short seriesAmount
+);
