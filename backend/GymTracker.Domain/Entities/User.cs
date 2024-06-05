b67a26c67909ca25fcf55eff5f2db490529ff3e6
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-06-04 21:14:01 -0300

    Changing uniqueness of some columns into database

--- backend/GymTracker.Domain/Entities/User.cs
+++ backend/GymTracker.Domain/Entities/User.cs
@@ -1,10 +1,12 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using GymTracker.Utils.Cryptography;
+using Microsoft.EntityFrameworkCore;
 
 namespace GymTracker.Domain.Entities;
 
 [Table("users")]
+[Index(nameof(Email), IsUnique = true)]
 public class User
 {
     [NotMapped]
