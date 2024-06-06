e86b280d7dffd93248e85e7ebd3b42b84a114ed4
eduardo <eduardopasqualetti9@gmail.com>
2024-06-06 09:07:11 -0300

    Login screen incremented

--- frontend/src/screens/LoginScreen/index.js
+++ frontend/src/screens/LoginScreen/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Button } from "../../components/Button";
 import { Container } from "../../components/Container/style";
 import Gradient from "../../components/Gradient";
@@ -9,15 +10,23 @@ import { LinkCommandText } from "./components/linkCommandText";
 import { LinkContainer } from "./components/linkContainer";
 
 export const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  async function handleLogin(){
+
+    navigation.navigate("Home")
+  }
+  
   return (
     <Gradient>
       <Container>
         <Logo marginTop={'20%'}/>
         <Title marginTop={'11%'}>Bem vindo</Title>  
-        <Input marginTop={'9%'} placeholder="E-mail ou usuário..."/>
-        <Input marginTop={'5%'} placeholder="Senha..." />
+        <Input marginTop={'9%'} placeholder="E-mail ou usuário..." value={email} onChangeText={setEmail}/>
+        <Input marginTop={'5%'} placeholder="Senha..." value={password} onChangeText={setPassword}/>
         <Link onPress={() => navigation.navigate("RecoverPasswordScreen")} textAlign={'right'} marginTop={'5%'}>Esqueceu sua senha ?</Link>
-        <Button marginTop={'35%'} title="Login" />
+        <Button marginTop={'35%'} title="Login" handleClickFn={handleLogin}/>
         <LinkContainer marginTop={'7%'}>
           <LinkCommandText>Não tem uma conta?</LinkCommandText>
           <Link onPress={() => navigation.navigate("NameRegisterScreen")}>Cadastre-se</Link>
