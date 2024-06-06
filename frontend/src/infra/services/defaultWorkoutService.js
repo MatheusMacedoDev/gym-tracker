e457cc936c7c692643173bab6fc79fff290a7726
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-06-06 14:53:48 -0300

    Get default workouts by user endpoint added to project

--- frontend/src/infra/services/defaultWorkoutService.js
+++ frontend/src/infra/services/defaultWorkoutService.js
@@ -1,15 +1,28 @@
 import api, { apiUrlLocal } from "../apiAccesor";
 
 const createDefaultWorkoutEndpoint = "/workouts/default_workout";
+const getDefaultWorkoutsByUserEndpoint = "/workouts/default_workout";
 
 export async function CreateDefaultWorkout(userId) {
   try {
-    const response = await api.get(
+    const response = await api.post(
       `${apiUrlLocal}${createDefaultWorkoutEndpoint}`,
       {
         userId,
         workoutName,
       },
+    );
+
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function GetDefaultWorkoutsByUserId(userId) {
+  try {
+    const response = await api.get(
+      `${apiUrlLocal}${createDefaultWorkoutEndpoint}/userId?=${userId}`,
     );
 
     return response;
