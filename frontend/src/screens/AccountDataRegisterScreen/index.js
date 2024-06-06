771ef4af19449a6f603cb0d67f64b48512a41818
eduardo <eduardopasqualetti9@gmail.com>
2024-06-06 09:14:14 -0300

    Account data screen incremented

--- frontend/src/screens/AccountDataRegisterScreen/index.js
+++ frontend/src/screens/AccountDataRegisterScreen/index.js
@@ -15,12 +15,17 @@ export const AccountDataRegisterScreen = ({ navigation, route }) => {
     const [password, setPassword] = useState("")
     const [confirmPassword, setConfirmPassword] = useState("")
 
+    async function handleRegister() {
+        if (password === confirmPassword) {
+            navigation.navigate("GenderRegisterScreen")
+        }
+    }
 
     return (
         <Gradient>
             <Container>
                 <IconButton
-                handleClickFn={() => navigation.navigate("NameRegisterScreen")}
+                    handleClickFn={() => navigation.navigate("NameRegisterScreen")}
                     gradient={false}
                     icon={
                         <MaterialIcons name="reply" size={40} color={'#FB6614'} />
@@ -28,12 +33,12 @@ export const AccountDataRegisterScreen = ({ navigation, route }) => {
                 />
                 <Logo marginTop={'25%'} />
                 <Title marginTop={'5%'}>Dados da Conta</Title>
-                <CommandText textAlign={'center'} marginTop={'10%'}>{ route.params != null ? route.params.name : null} por favor, preencha os seus dados  para criar sua nova conta:</CommandText>
-                <Input marginTop={'20%'} placeholder="E-mail..." value={email} onChangeText={setEmail}/>
-                <Input marginTop={'5%'} placeholder="Senha..."  value={password} onChangeText={setPassword}/>
-                <Input marginTop={'5%'} placeholder="Confirme sua senha..."  value={confirmPassword} onChangeText={setConfirmPassword}/>
+                <CommandText textAlign={'center'} marginTop={'10%'}>{route.params != null ? route.params.name : null} por favor, preencha os seus dados  para criar sua nova conta:</CommandText>
+                <Input marginTop={'15%'} placeholder="E-mail..." value={email} onChangeText={setEmail} />
+                <Input marginTop={'5%'} placeholder="Senha..." value={password} onChangeText={setPassword} />
+                <Input marginTop={'5%'} placeholder="Confirme sua senha..." value={confirmPassword} onChangeText={setConfirmPassword} />
                 <Button
-                    handleClickFn={() => navigation.navigate("GenderRegisterScreen")}
+                    handleClickFn={handleRegister}
                     marginTop={'15%'}
                     title="Continuar"
                     icon={(size, color) => (
