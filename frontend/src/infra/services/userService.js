67ca8db906e3fb1d9c2233c47eeba3aac3bc578e
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-06-06 09:00:21 -0300

    Register endpoint created

--- frontend/src/infra/services/userService.js
+++ frontend/src/infra/services/userService.js
@@ -1,12 +1,33 @@
 import api from "../ApiAccesor"
 
-const loginEndpoint = '/users/login'
+const registerUserEndpoint = '/users';
+const loginEndpoint = '/users/login';
 
 export function makeLogin(email, password) {
-    const response = api.post(loginEndpoint, {
-        userEmail: email,
-        userPassword: password
-    })
+    try {
+        const response = api.post(loginEndpoint, {
+            userEmail: email,
+            userPassword: password
+        })
+    
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-    return response;
+export function registerUser(email, password, name, birthYear, gender) {
+    try {
+        const response = api.post(registerUserEndpoint, {
+            email,
+            password,
+            name,
+            birthYear,
+            gender
+        });
+    
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
 }
