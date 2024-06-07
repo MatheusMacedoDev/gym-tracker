af2bd4cdb8595384912f0225825bf494b57ddf53
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-06-07 19:45:55 -0300

    EmailCodeScreen fixed for Android

--- frontend/src/screens/EmailCodeScreen/index.js
+++ frontend/src/screens/EmailCodeScreen/index.js
@@ -1,50 +1,65 @@
-import { useState } from "react"
-import { Button } from "../../components/Button"
-import CodeInput from "../../components/CodeInput"
-import { CommandText } from "../../components/CommandText/style"
-import { Container } from "../../components/Container/style"
-import { IconButton } from "../../components/IconButton"
-import { Title } from "../../components/Title/style"
+import { useState } from 'react';
+import { Button } from '../../components/Button';
+import CodeInput from '../../components/CodeInput';
+import { CommandText } from '../../components/CommandText/style';
+import { Container } from '../../components/Container/style';
+import { IconButton } from '../../components/IconButton';
+import { Title } from '../../components/Title/style';
 import { Entypo } from '@expo/vector-icons';
-import { MaterialIcons } from "@expo/vector-icons";
-import { Keyboard, TouchableWithoutFeedback } from "react-native"
-import { Logo } from "../../components/Logo"
-import Gradient from "../../components/Gradient"
+import { MaterialIcons } from '@expo/vector-icons';
+import { Keyboard, TouchableWithoutFeedback } from 'react-native';
+import { Logo } from '../../components/Logo';
+import Gradient from '../../components/Gradient';
+import { percentage } from '../../utils/percentageFactory';
 
 export const EmailCodeScreen = ({ navigation }) => {
     const [code, setCode] = useState('');
 
-
     async function passToResetPassword() {
-        navigation.navigate("ResetPasswordScreen")
+        navigation.navigate('ResetPasswordScreen');
     }
-
 
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
             <Gradient>
                 <Container>
                     <IconButton
-                        handleClickFn={() => navigation.navigate("RecoverPasswordScreen")}
+                        handleClickFn={() => navigation.goBack()}
                         gradient={false}
                         icon={
-                            <MaterialIcons name="reply" size={40} color={'#FB6614'} />
+                            <MaterialIcons
+                                name='reply'
+                                size={40}
+                                color={'#FB6614'}
+                            />
                         }
                     />
-                    <Logo marginTop={'30%'} />
-                    <Title marginTop={'10%'}>Código de verificação</Title>
-                    <CommandText marginTop={'10%'} marginBottom={70}>Digite o cógido que foi enviado para o seu e-mail para verficarmos o seu pedido.</CommandText>
+                    <Logo marginTop={percentage(0.15, 'h')} />
+                    <Title marginTop={percentage(0.05, 'h')}>
+                        Código de verificação
+                    </Title>
+                    <CommandText
+                        marginTop={percentage(0.05, 'h')}
+                        marginBottom={percentage(0.12, 'h')}
+                    >
+                        Digite o cógido que foi enviado para o seu e-mail para
+                        verficarmos o seu pedido.
+                    </CommandText>
                     <CodeInput code={code} setCode={setCode} />
                     <Button
                         handleClickFn={passToResetPassword}
-                        marginTop={'35%'}
-                        title="Continuar"
+                        marginTop={percentage(0.12, 'h')}
+                        title='Continuar'
                         icon={(size, color) => (
-                            <Entypo name="chevron-right" size={size} color={color} />
+                            <Entypo
+                                name='chevron-right'
+                                size={size}
+                                color={color}
+                            />
                         )}
                     />
                 </Container>
             </Gradient>
         </TouchableWithoutFeedback>
-    )
-}
+    );
+};
