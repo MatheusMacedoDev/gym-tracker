f6d67201b50a1fd7fa5caa8fe5fc5bdd666a5281
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-06-07 14:37:32 -0300

    BirthYearRegisterScreen fixed for Android

--- frontend/src/components/SelectPicker/ScrollPicker/index.js
+++ frontend/src/components/SelectPicker/ScrollPicker/index.js
@@ -1,107 +1,115 @@
-import React from "react";
+import React from 'react';
 import {
-  FlatList,
-  View,
-  TouchableOpacity,
-  Text,
-  StyleSheet,
-} from "react-native";
+    FlatList,
+    View,
+    TouchableOpacity,
+    Text,
+    StyleSheet
+} from 'react-native';
+import { percentage } from '../../../utils/percentageFactory';
 
-const LIGHT_GRAY = "#DEE2EE";
+const LIGHT_GRAY = '#DEE2EE';
 
 const ScrollPicker = ({
-  list,
-  extraData,
-  onItemPress,
-  currentValue,
-  initialNumToRender,
-  labelColor,
-  separatorColor,
-  selectedColor,
+    list,
+    extraData,
+    onItemPress,
+    currentValue,
+    initialNumToRender,
+    labelColor,
+    separatorColor,
+    selectedColor
 }) => {
-  const pickedIndex = list.findIndex((item) => item.value === currentValue);
-  const ItemHeight = 39.8;
-  const LABEL_COLOR = labelColor || "black";
-  const SELECTED_COLOR = selectedColor || "blue";
-  const SEPARATOR_COLOR = separatorColor || LIGHT_GRAY;
+    const pickedIndex = list.findIndex(item => item.value === currentValue);
+    const ItemHeight = 39.8;
+    const LABEL_COLOR = labelColor || 'black';
+    const SELECTED_COLOR = selectedColor || 'blue';
+    const SEPARATOR_COLOR = separatorColor || LIGHT_GRAY;
 
-  const FlatListItemSeparator = () => {
-    return (
-      // Item Separator
-      <View
-        style={{ height: 3, width: "100%", backgroundColor: SEPARATOR_COLOR }}
-      />
-    );
-  };
-
-  const Row = ({ value, label, index }) => {
-    return (
-      <TouchableOpacity style={styles.Row} onPress={() => onItemPress(value)}>
-        {pickedIndex === index ? (
-          <Text
-            style={{
-              fontSize: 45,
-              textAlign: "center",
-              fontFamily: "Montserrat_700Bold",
-              color: SELECTED_COLOR,
-            }}
-          >{`${label}`}</Text>
-        ) : (
-          <Text style={{ ...styles.RowText, color: LABEL_COLOR }}>
-            {`${label}`}
-          </Text>
-        )}
-      </TouchableOpacity>
-    );
-  };
-
-  return (
-    <>
-      <View style={styles.Container}>
-        <FlatList
-          data={list}
-          extraData={extraData}
-          initialNumToRender={initialNumToRender}
-          renderItem={({ item, index }) => (
-            <Row
-              value={item.value}
-              label={item.label}
-              index={index}
-              isSelected={item.value ? item.value : false}
+    const FlatListItemSeparator = () => {
+        return (
+            // Item Separator
+            <View
+                style={{
+                    height: 3,
+                    width: '100%',
+                    backgroundColor: SEPARATOR_COLOR
+                }}
             />
-          )}
-          keyExtractor={(item) => item.label}
-          getItemLayout={(data, index) => ({
-            length: ItemHeight,
-            offset: ItemHeight * index,
-            index,
-          })}
-          initialScrollIndex={pickedIndex - 3}
-        />
-      </View>
-    </>
-  );
+        );
+    };
+
+    const Row = ({ value, label, index }) => {
+        return (
+            <TouchableOpacity
+                style={styles.Row}
+                onPress={() => onItemPress(value)}
+            >
+                {pickedIndex === index ? (
+                    <Text
+                        style={{
+                            fontSize: 45,
+                            textAlign: 'center',
+                            fontFamily: 'Montserrat_700Bold',
+                            color: SELECTED_COLOR
+                        }}
+                    >{`${label}`}</Text>
+                ) : (
+                    <Text style={{ ...styles.RowText, color: LABEL_COLOR }}>
+                        {`${label}`}
+                    </Text>
+                )}
+            </TouchableOpacity>
+        );
+    };
+
+    return (
+        <>
+            <View style={styles.Container}>
+                <FlatList
+                    data={list}
+                    extraData={extraData}
+                    initialNumToRender={initialNumToRender}
+                    renderItem={({ item, index }) => (
+                        <Row
+                            value={item.value}
+                            label={item.label}
+                            index={index}
+                            isSelected={item.value ? item.value : false}
+                        />
+                    )}
+                    keyExtractor={item => item.label}
+                    getItemLayout={(data, index) => ({
+                        length: ItemHeight,
+                        offset: ItemHeight * index,
+                        index
+                    })}
+                    initialScrollIndex={pickedIndex - 3}
+                />
+            </View>
+        </>
+    );
 };
 
 const styles = StyleSheet.create({
-  Row: {
-    height: 80,
-    width: "100%",
-    paddingTop: 6,
-    paddingBottom: 8,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  RowText: {
-    fontSize: 25,
-    textAlign: "center",
-    fontFamily: "Montserrat_700Bold",
-  },
-  Container: {
-    width: "100%",
-    height: '25%',
-    marginTop: 100
-  },
+    Row: {
+        height: 80,
+        width: '100%',
+        paddingTop: 6,
+        paddingBottom: 8,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    RowText: {
+        fontSize: 25,
+        textAlign: 'center',
+        fontFamily: 'Montserrat_700Bold'
+    },
+    Container: {
+        width: '100%',
+        height: '25%',
+        marginTop: 64
+    }
 });
 
 export default ScrollPicker;
--- frontend/src/screens/BirthYearRegisterScreen/index.js
+++ frontend/src/screens/BirthYearRegisterScreen/index.js
@@ -1,59 +1,78 @@
-import { useEffect, useState } from "react"
-import { Button } from "../../components/Button"
-import {  Container } from "../../components/Container/style"
-import { IconButton } from "../../components/IconButton"
-import { Logo } from "../../components/Logo"
-import { Title } from "../../components/Title/style"
+import { useEffect, useState } from 'react';
+import { Button } from '../../components/Button';
+import { Container } from '../../components/Container/style';
+import { IconButton } from '../../components/IconButton';
+import { Logo } from '../../components/Logo';
+import { Title } from '../../components/Title/style';
 import { Entypo } from '@expo/vector-icons';
-import { MaterialIcons } from "@expo/vector-icons";
-import { createArrayYears } from "../../utils/arraysFactory.js"
-import Gradient from "../../components/Gradient/index.js"
-import { SelectPicker } from "../../components/SelectPicker/index.js"
-import { makeLogin, registerUser } from "../../infra/services/userService.js"
+import { MaterialIcons } from '@expo/vector-icons';
+import { createArrayYears } from '../../utils/arraysFactory.js';
+import Gradient from '../../components/Gradient/index.js';
+import { SelectPicker } from '../../components/SelectPicker/index.js';
+import { registerUser } from '../../infra/services/userService.js';
+import { percentage } from '../../utils/percentageFactory.js';
 
 export const BirthYearRegisterScreen = ({ navigation, route }) => {
-    const [yearBirth, setYearBirth] = useState("");
-    const [userData, setUserData] = useState({})
+    const [yearBirth, setYearBirth] = useState('');
+    const [userData, setUserData] = useState({});
 
-     useEffect(() => {
-         setUserData(route.params.userData)
-         console.log(route.params);
-     },[])
+    useEffect(() => {
+        setUserData(route.params.userData);
+        console.log(route.params);
+    }, []);
 
     async function RegisterUser() {
-        const response = await registerUser(userData.email,userData.password,userData.name, parseInt(yearBirth), userData.gender)
+        const response = await registerUser(
+            userData.email,
+            userData.password,
+            userData.name,
+            parseInt(yearBirth),
+            userData.gender
+        );
         console.log(response.status);
         if (response.status === 201) {
-            navigation.replace("Login")
+            navigation.replace('Login');
         }
     }
 
     return (
         <Gradient>
             <Container>
-            <IconButton
-            handleClickFn={() => navigation.navigate("GenderRegisterScreen")}
-                gradient={false}
-                icon={
-                    <MaterialIcons name="reply" size={40} color={'#FB6614'} />
-                }
-            />
-            <Logo marginTop={'30%'} />
-            <Title marginTop={'10%'}>Qual o seu ano de nascimento?</Title>
-            <SelectPicker
-                onItemPress={setYearBirth}
-                valueNow={yearBirth}
-                list={createArrayYears()}
-            /> 
-            <Button
-                handleClickFn={RegisterUser}
-                marginTop={'20%'}
-                title="Continuar"
-                icon={(size, color) => (
-                    <Entypo name="chevron-right" size={size} color={color} />
-                )}
-            />
+                <IconButton
+                    handleClickFn={() =>
+                        navigation.navigate('GenderRegisterScreen')
+                    }
+                    gradient={false}
+                    icon={
+                        <MaterialIcons
+                            name='reply'
+                            size={40}
+                            color={'#FB6614'}
+                        />
+                    }
+                />
+                <Logo marginTop={percentage(0.15, 'h')} />
+                <Title marginTop={percentage(0.05, 'h')}>
+                    Qual o seu ano de nascimento?
+                </Title>
+                <SelectPicker
+                    onItemPress={setYearBirth}
+                    valueNow={yearBirth}
+                    list={createArrayYears()}
+                />
+                <Button
+                    handleClickFn={RegisterUser}
+                    marginTop={percentage(0.11, 'h')}
+                    title='Continuar'
+                    icon={(size, color) => (
+                        <Entypo
+                            name='chevron-right'
+                            size={size}
+                            color={color}
+                        />
+                    )}
+                />
             </Container>
         </Gradient>
-    )
-}
+    );
+};
