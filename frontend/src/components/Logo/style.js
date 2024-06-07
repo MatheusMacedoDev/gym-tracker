23883f0a8b758603fc1f08ecd9e1c0fdb4715e7b
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-06-07 19:20:24 -0300

    RecoverPasswordScreen fixed for Android

--- frontend/src/components/Logo/style.js
+++ frontend/src/components/Logo/style.js
@@ -1,9 +1,10 @@
-import styled from "styled-components/native";
+import styled from 'styled-components/native';
 
 export const PhotoLogo = styled.Image`
-    height: ${props => props.heightLogo ? `${props.heightLogo}`: "53px"};
-    width: ${props => props.widthLogo ? `${props.widthLogo}` : "109px"};
+    height: ${props => (props.heightLogo ? `${props.heightLogo}` : '53px')};
+    width: ${props => (props.widthLogo ? `${props.widthLogo}` : '109px')};
     align-self: center;
-    margin-top: ${props => props.marginTop ? `${props.marginTop}`: "0px"};
-    margin-bottom: ${props => props.marginBottom  ? `${props.marginBottom}` : "0px"};
-`; 
+    margin-top: ${props => (props.marginTop ? `${props.marginTop}` : '0px')};
+    margin-bottom: ${props =>
+        props.marginBottom ? `${props.marginBottom}` : '0px'};
+`;
--- frontend/src/screens/RecoverPasswordScreen/index.js
+++ frontend/src/screens/RecoverPasswordScreen/index.js
@@ -1,44 +1,60 @@
-import { Image } from "react-native"
-import { Button } from "../../components/Button"
-import { CommandText } from "../../components/CommandText/style"
-import { Container } from "../../components/Container/style"
-import { IconButton } from "../../components/IconButton"
-import { Input } from "../../components/Input/style"
-import { Logo } from "../../components/Logo"
-import { Title } from "../../components/Title/style"
+import { Image } from 'react-native';
+import { Button } from '../../components/Button';
+import { CommandText } from '../../components/CommandText/style';
+import { Container } from '../../components/Container/style';
+import { IconButton } from '../../components/IconButton';
+import { Input } from '../../components/Input/style';
+import { Logo } from '../../components/Logo';
+import { Title } from '../../components/Title/style';
 import { Entypo } from '@expo/vector-icons';
-import { MaterialIcons } from "@expo/vector-icons";
-import Gradient from "../../components/Gradient"
+import { MaterialIcons } from '@expo/vector-icons';
+import Gradient from '../../components/Gradient';
+import { percentage } from '../../utils/percentageFactory';
 
 export const RecoverPasswordScreen = ({ navigation }) => {
-
     async function passToEmailCode() {
-        navigation.navigate("EmailCodeScreen")
+        navigation.navigate('EmailCodeScreen');
     }
 
     return (
         <Gradient>
             <Container>
                 <IconButton
-                handleClickFn={() => navigation.navigate("LoginScreen")}
+                    handleClickFn={() => navigation.goBack()}
                     gradient={false}
                     icon={
-                        <MaterialIcons name="reply" size={40} color={'#FB6614'} />
+                        <MaterialIcons
+                            name='reply'
+                            size={40}
+                            color={'#FB6614'}
+                        />
                     }
                 />
-                <Logo marginTop={'30%'} />
-                <Title marginTop={'10%'}>Esqueceu a senha?</Title>
-                <CommandText marginTop={'10%'}>Digite o seu e-mail para que possamos enviar um código de recuperação.</CommandText>
-                <Input marginTop={'20%'} placeholder="Email de recuperação..." />
+                <Logo marginTop={percentage(0.15, 'h')} />
+                <Title marginTop={percentage(0.05, 'h')}>
+                    Esqueceu a senha?
+                </Title>
+                <CommandText marginTop={percentage(0.05, 'h')}>
+                    Digite o seu e-mail para que possamos enviar um código de
+                    recuperação.
+                </CommandText>
+                <Input
+                    marginTop={percentage(0.12, 'h')}
+                    placeholder='Email de recuperação...'
+                />
                 <Button
                     handleClickFn={passToEmailCode}
-                    marginTop={'35%'}
-                    title="Continuar"
+                    marginTop={percentage(0.12, 'h')}
+                    title='Continuar'
                     icon={(size, color) => (
-                        <Entypo name="chevron-right" size={size} color={color} />
+                        <Entypo
+                            name='chevron-right'
+                            size={size}
+                            color={color}
+                        />
                     )}
                 />
             </Container>
         </Gradient>
-    )
-}
+    );
+};
