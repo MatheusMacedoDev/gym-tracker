7a9d16ed59d700dc0d93dca802a27b5ebfc8d081
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-06-07 19:49:58 -0300

    ResetPasswordScreen fixed for Android

--- frontend/src/screens/ResetPasswordScreen/index.js
+++ frontend/src/screens/ResetPasswordScreen/index.js
@@ -1,40 +1,61 @@
-import { Button } from "../../components/Button"
-import { CommandText } from "../../components/CommandText/style"
-import { Container } from "../../components/Container/style"
-import Gradient from "../../components/Gradient"
-import { IconButton } from "../../components/IconButton"
-import { Input } from "../../components/Input/style"
-import { Logo } from "../../components/Logo"
-import { Title } from "../../components/Title/style"
+import { Button } from '../../components/Button';
+import { CommandText } from '../../components/CommandText/style';
+import { Container } from '../../components/Container/style';
+import Gradient from '../../components/Gradient';
+import { IconButton } from '../../components/IconButton';
+import { Input } from '../../components/Input/style';
+import { Logo } from '../../components/Logo';
+import { Title } from '../../components/Title/style';
 import { Entypo } from '@expo/vector-icons';
-import { MaterialIcons } from "@expo/vector-icons";
+import { MaterialIcons } from '@expo/vector-icons';
+import { percentage } from '../../utils/percentageFactory';
 
 export const ResetPasswordScreen = ({ navigation }) => {
-
     return (
         <Gradient>
             <Container>
                 <IconButton
-                    handleClickFn={() => { navigation.navigate("EmailCodeScreen") }}
+                    handleClickFn={() => {
+                        navigation.goBack();
+                    }}
                     gradient={false}
                     icon={
-                        <MaterialIcons name="reply" size={40} color={'#FB6614'} />
+                        <MaterialIcons
+                            name='reply'
+                            size={40}
+                            color={'#FB6614'}
+                        />
                     }
                 />
-                <Logo marginTop={'30%'} />
-                <Title marginTop={'10%'}>Quase lá</Title>
-                <CommandText textAlign={'center'} marginTop={'10%'}>Altere sua senha</CommandText>
-                <Input marginTop={'18%'} placeholder="Nova senha..." />
-                <Input marginTop={'10%'} placeholder="Repita a nova senha..." />
+                <Logo marginTop={percentage(0.15, 'h')} />
+                <Title marginTop={percentage(0.05, 'h')}>Quase lá</Title>
+                <CommandText
+                    textAlign={'center'}
+                    marginTop={percentage(0.05, 'h')}
+                >
+                    Altere sua senha
+                </CommandText>
+                <Input
+                    marginTop={percentage(0.12, 'h')}
+                    placeholder='Nova senha...'
+                />
+                <Input
+                    marginTop={percentage(0.03, 'h')}
+                    placeholder='Repita a nova senha...'
+                />
                 <Button
-                    marginTop={'30%'}
-                    title="Continuar"
+                    marginTop={percentage(0.12, 'h')}
+                    title='Continuar'
                     icon={(size, color) => (
-                        <Entypo name="chevron-right" size={size} color={color} />
+                        <Entypo
+                            name='chevron-right'
+                            size={size}
+                            color={color}
+                        />
                     )}
-                    handleClickFn={() => navigation.navigate("LoginScreen")}
+                    handleClickFn={() => navigation.navigate('LoginScreen')}
                 />
             </Container>
         </Gradient>
-    )
-}
+    );
+};
