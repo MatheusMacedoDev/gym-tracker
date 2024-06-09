8a01d9b486bc5d174b42e63fd1390a079ff46346
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-06-08 22:29:59 -0300

    ExerciseRecordScreen fixed for Android

--- frontend/src/components/List/style.js
+++ frontend/src/components/List/style.js
@@ -1,6 +1,6 @@
-import { FlatList } from "react-native";
-import styled from "styled-components/native";
+import { FlatList } from 'react-native';
+import styled from 'styled-components/native';
 
 export const ListComponent = styled(FlatList)`
-    width: 100% ;
-`
+    width: 100%;
+`;
--- frontend/src/components/SelectPicker/index.js
+++ frontend/src/components/SelectPicker/index.js
@@ -1,20 +1,15 @@
-import { useEffect, useState } from "react";
-import { Dimensions, StyleSheet, Text, View } from "react-native";
-import ScrollPicker from "./ScrollPicker";
-import { colors } from "../../colors.config";
+import ScrollPicker from './ScrollPicker';
+import { colors } from '../../colors.config';
 
-export const SelectPicker = ({onItemPress, valueNow, list}) => {
-
-  return (
-    <ScrollPicker
-        list={list}
-        onItemPress={onItemPress}
-        labelColor="white"
-        separatorColor="white"
-        selectedColor= {colors.orange}
-        currentValue={valueNow}
-    />
-  );
+export const SelectPicker = ({ onItemPress, valueNow, list }) => {
+    return (
+        <ScrollPicker
+            list={list}
+            onItemPress={onItemPress}
+            labelColor='white'
+            separatorColor='white'
+            selectedColor={colors.orange}
+            currentValue={valueNow}
+        />
+    );
 };
-
-
