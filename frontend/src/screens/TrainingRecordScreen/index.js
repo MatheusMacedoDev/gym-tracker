6d16ba5cc4ab32221d64f4673ac2c2267cd7bf50
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-06-08 21:30:35 -0300

    TrainingRecordScreenScreen fixed for Android

--- frontend/src/screens/TrainingRecordScreen/index.js
+++ frontend/src/screens/TrainingRecordScreen/index.js
@@ -1,72 +1,123 @@
-import { Container } from "../../components/Container/style";
-import { IconButton } from "../../components/IconButton";
-import { MaterialIcons } from "@expo/vector-icons";
-import { Logo } from "../../components/Logo";
-import { Title } from "../../components/Title/style";
-import { CommandText } from "../../components/CommandText/style";
-import { CardWorkout } from "../../components/CardWorkout";
-import { Button } from "../../components/Button";
-import { Entypo } from "@expo/vector-icons";
-import { ListComponent } from "../../components/List/style";
-import { ListContainer } from "../../components/ListContainer/style";
-import { useState } from "react";
-import { TouchableOpacity } from "react-native";
-import Gradient from "../../components/Gradient";
+import { Container } from '../../components/Container/style';
+import { IconButton } from '../../components/IconButton';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Logo } from '../../components/Logo';
+import { Title } from '../../components/Title/style';
+import { CommandText } from '../../components/CommandText/style';
+import { CardWorkout } from '../../components/CardWorkout';
+import { Button } from '../../components/Button';
+import { Entypo } from '@expo/vector-icons';
+import { ListComponent } from '../../components/List/style';
+import { ListContainer } from '../../components/ListContainer/style';
+import { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import Gradient from '../../components/Gradient';
+import { percentage } from '../../utils/percentageFactory';
 
 const workouts = [
-  { id: 1, trainingName: "Treino A", muscleGroups: "Peito - Triceps - costas" },
-  { id: 2, trainingName: "Treino B", muscleGroups: "Peito - Triceps - costas" },
-  { id: 3, trainingName: "Treino C", muscleGroups: "Peito - Triceps - costas" },
-  { id: 4, trainingName: "Treino D", muscleGroups: "Peito - Triceps - costas" },
-  { id: 5, trainingName: "Treino E", muscleGroups: "Peito - Triceps - costas" },
+    {
+        id: 1,
+        trainingName: 'Treino A',
+        muscleGroups: 'Peito - Triceps - costas'
+    },
+    {
+        id: 2,
+        trainingName: 'Treino B',
+        muscleGroups: 'Peito - Triceps - costas'
+    },
+    {
+        id: 3,
+        trainingName: 'Treino C',
+        muscleGroups: 'Peito - Triceps - costas'
+    },
+    {
+        id: 4,
+        trainingName: 'Treino D',
+        muscleGroups: 'Peito - Triceps - costas'
+    },
+    {
+        id: 5,
+        trainingName: 'Treino E',
+        muscleGroups: 'Peito - Triceps - costas'
+    }
 ];
 
 export const TrainingRecordScrenn = ({ navigation }) => {
-  const [selectedWorkout, setSelectedWorkout] = useState();
+    const [selectedWorkout, setSelectedWorkout] = useState();
 
-  return (
-    <Gradient>
-      <Container>
-        <IconButton
-          gradient={false}
-          onPress={() => navigation.goBack()}
-          icon={<MaterialIcons name="reply" size={40} color={"#FB6614"} 
-          onPress={() => navigation.goBack()}/>} 
-        />
-        <Logo marginTop={"20%"} />
-        <Title marginTop={"10%"}>Registro de treino</Title>
-        <CommandText marginTop={"5%"} textAlign={"center"} marginBottom={"10%"}>
-          Aqui você poderá anotar os principais dados do seu treino para
-          podermos medir a sua progressão. Antes de tudo selecione o seu treino
-          base:
-        </CommandText>
-        <ListContainer>
-          <ListComponent
-            data={workouts}
-            renderItem={({ item }) => (
-                <TouchableOpacity onPress={() => {setSelectedWorkout({
-                    id: item.id,
-                    trainingName: item.trainingName
-                })}}>
-              <CardWorkout
-                trainingName={item.trainingName}
-                muscleGroups={item.muscleGroups}
-                marginBottom={"5%"}
-                isSelected={selectedWorkout ? item.id == selectedWorkout.id : false}
-              />
-              </TouchableOpacity>
-            )}
-          />
-        </ListContainer>
-        <Button
-          marginTop={"10%"}
-          title="Continuar"
-          icon={(size, color) => (
-            <Entypo name="chevron-right" size={size} color={color} />
-          )}
-          handleClickFn={() => navigation.navigate("TrainingExercisesScreens", {trainingName: selectedWorkout.trainingName})}
-        />
-      </Container>
-    </Gradient>
-  );
+    return (
+        <Gradient>
+            <Container>
+                <IconButton
+                    gradient={false}
+                    onPress={() => navigation.goBack()}
+                    icon={
+                        <MaterialIcons
+                            name='reply'
+                            size={40}
+                            color={'#FB6614'}
+                            onPress={() => navigation.goBack()}
+                        />
+                    }
+                />
+                <Logo marginTop={percentage(0.1, 'h')} />
+                <Title marginTop={percentage(0.03, 'h')}>
+                    Registro de treino
+                </Title>
+                <CommandText
+                    marginTop={percentage(0.03, 'h')}
+                    textAlign={'center'}
+                    marginBottom={percentage(0.05, 'h')}
+                >
+                    Aqui você poderá anotar os principais dados do seu treino
+                    para podermos medir a sua progressão. Antes de tudo
+                    selecione o seu treino base:
+                </CommandText>
+                <ListContainer>
+                    <ListComponent
+                        data={workouts}
+                        contentContainerStyle={{
+                            gap: 12
+                        }}
+                        renderItem={({ item }) => (
+                            <TouchableOpacity
+                                onPress={() => {
+                                    setSelectedWorkout({
+                                        id: item.id,
+                                        trainingName: item.trainingName
+                                    });
+                                }}
+                            >
+                                <CardWorkout
+                                    trainingName={item.trainingName}
+                                    muscleGroups={item.muscleGroups}
+                                    isSelected={
+                                        selectedWorkout
+                                            ? item.id == selectedWorkout.id
+                                            : false
+                                    }
+                                />
+                            </TouchableOpacity>
+                        )}
+                    />
+                </ListContainer>
+                <Button
+                    marginTop={percentage(0.05, 'h')}
+                    title='Continuar'
+                    icon={(size, color) => (
+                        <Entypo
+                            name='chevron-right'
+                            size={size}
+                            color={color}
+                        />
+                    )}
+                    handleClickFn={() =>
+                        navigation.navigate('TrainingExercisesScreens', {
+                            trainingName: selectedWorkout.trainingName
+                        })
+                    }
+                />
+            </Container>
+        </Gradient>
+    );
 };
