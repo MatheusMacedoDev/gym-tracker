d625b54000aa2069dbb9d714d5f6989391f005c3
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-06-10 20:34:51 -0300

    Percentage factory changed to have no more hooks

--- frontend/src/utils/percentageFactory.js
+++ frontend/src/utils/percentageFactory.js
@@ -1,9 +1,8 @@
-import React from "react";
-import { useWindowDimensions }  from "react-native";
-
+import { Dimensions } from 'react-native';
 
 export function percentage(percentageAmount, axis) {
-    const { width, height } = useWindowDimensions();
+    const width = Dimensions.get('window').width;
+    const height = Dimensions.get('window').height;
 
     axis = axis.toLowerCase();
 
@@ -12,6 +11,6 @@ export function percentage(percentageAmount, axis) {
     }
 
     if (axis === 'h') {
-        return `${Math.round(percentageAmount * height)}px`
+        return `${Math.round(percentageAmount * height)}px`;
     }
 }
