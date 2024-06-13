6a49f0dbe158f20597e67ba87024ab764000d9d3
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-06-12 22:24:57 -0300

    Login and registration input improved

--- frontend/src/components/Input/style.js
+++ frontend/src/components/Input/style.js
@@ -1,15 +1,14 @@
 import styled from 'styled-components/native';
 import { colors } from '../../colors.config';
 
-
 export const Input = styled.TextInput.attrs({
     placeholderTextColor: colors.lightGray
 })`
-    height: ${props => props.heightInput ? `${props.heightInput}`: "8%"};
-    width: ${props => props.widthInput ? `${props.widthInput}` : "100%"};
+    height: ${props => (props.heightInput ? `${props.heightInput}` : '64px')};
+    width: ${props => (props.widthInput ? `${props.widthInput}` : '100%')};
     font-size: 16px;
     background-color: ${colors.darkGray};
-    color:${colors.white};
+    color: ${colors.white};
     border-radius: 10px;
     padding-left: 17px;
     margin-top: ${props => `${props.marginTop}` || '0px'};
