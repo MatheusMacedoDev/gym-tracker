5221f0f28fb67d4ea108814788b6ec328f582fba
Rubens Moura Cavalcanti <50496884859@info.local>
2024-06-13 10:51:35 -0300

    Trying to validate login screen

--- frontend/src/screens/LoginScreen/index.js
+++ frontend/src/screens/LoginScreen/index.js
@@ -12,16 +12,35 @@ import { MakeLogin } from '../../infra/services/userService';
 import { percentage } from '../../utils/percentageFactory';
 import { getUserToken, setUserToken } from '../../utils/tokenHandler';
 import AuthContext from '../../global/AuthContext';
+import { useForm } from 'react-hook-form';
+import * as yup from "yup";
 
 export const LoginScreen = ({ navigation }) => {
     const { user, setUser } = useContext(AuthContext);
 
     const [email, setEmail] = useState('matheus@mail.com');
     const [password, setPassword] = useState('12345');
+    
+    const fieldsValidationSchema = yup.object().shape({
+        email: yup
+        .string()
+        .required('O email não pode ser vazio')
+        .email('Digite um email válido'),
+        password: yup
+        .string()
+        .required('A senha não pode ser vazia')
+        .min(6, 'A senha deve conter pelo menos 6 dígitos')
+      })
+      
+      const { register, setValue, handleSubmit, errors } = useForm({ validationSchema: fieldsValidationSchema })
 
-    async function handleLogin() {
-        const response = await MakeLogin(email, password);
+    useEffect(() => {
+        register('email')
+        register('password')
+    }, [register])
 
+    async function handleLogin(data) {
+        const response = await MakeLogin(data.email, data.password);
         if (response.status == 200) {
             const token = response.data.authenticationToken;
 
@@ -43,17 +62,15 @@ export const LoginScreen = ({ navigation }) => {
                 <Title marginTop={percentage(0.05, 'h')}>Bem vindo</Title>
                 <Input
                     marginTop={percentage(0.12, 'h')}
-                    placeholder='E-mail ou usuário...'
-                    value={email}
-                    onChangeText={setEmail}
+                    placeholder='E-mail...'
+                    onChangeText={text => setValue('email', text)}
                     autoFocus
                 />
                 <Input
                     marginTop={percentage(0.03, 'h')}
                     placeholder='Senha...'
                     secureTextEntry
-                    value={password}
-                    onChangeText={setPassword}
+                    onChangeText={text => setValue('password', text)}
                 />
                 <Link
                     onPress={() => navigation.navigate('RecoverPasswordScreen')}
@@ -65,7 +82,7 @@ export const LoginScreen = ({ navigation }) => {
                 <Button
                     marginTop={percentage(0.1, 'h')}
                     title='Login'
-                    handleClickFn={handleLogin}
+                    handleClickFn={handleSubmit(handleLogin)}
                 />
                 <LinkContainer marginTop={percentage(0.04, 'h')}>
                     <LinkCommandText>Não tem uma conta?</LinkCommandText>
