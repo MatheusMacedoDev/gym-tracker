40efb3be891b37660f51499bb2e2525efa37492c
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-06-17 16:49:00 -0300

    Navigation bar bug fixed

--- frontend/src/configs/navigationBarConfig.js
+++ frontend/src/configs/navigationBarConfig.js
@@ -2,16 +2,11 @@ import * as NavigationBar from 'expo-navigation-bar';
 import { Platform } from 'react-native';
 
 export function configureNavigationBar() {
-    NavigationBar.setBackgroundColorAsync('#0E0C0F');
-    NavigationBar.setBehaviorAsync('inset-swipe');
-    NavigationBar.setVisibilityAsync('hidden');
-    // NavigationBar.addVisibilityListener(event => {
-    //     const timeToHidden = 5000;
-
-    //     if (event.visibility === 'visible') {
-    //         setTimeout(() => {
-    //             NavigationBar.setVisibilityAsync('visible');
-    //         }, timeToHidden);
-    //     }
-    // });
+    if (Platform.OS === 'android') {
+        NavigationBar.setBackgroundColorAsync('#0E0C0F');
+        NavigationBar.setBehaviorAsync('inset-swipe');
+        NavigationBar.setVisibilityAsync('hidden');
+    } else {
+        console.log('nao funciona em ios');
+    }
 }
