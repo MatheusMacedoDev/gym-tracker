af13aebd010abe8b5ac6b3394b35ef0934ea4126
Gabriela Rosa <gabii.ramos1309@gmail.com>
2024-06-18 08:39:08 -0300

    Everything fixed

--- frontend/src/screens/SelectExercise/index.js
+++ frontend/src/screens/SelectExercise/index.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Title } from '../../components/Title/style';
 import { IconButton } from '../../components/IconButton';
 import { MaterialIcons } from '@expo/vector-icons';
-import { FlatList, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { FlatList, TouchableOpacity } from 'react-native';
 import Gradient from '../../components/Gradient';
 import { Container } from '../../components/Container/style';
 import { ListContainer } from '../../components/ListContainer/style';
@@ -13,17 +13,18 @@ import { ExerciseCard } from '../../components/ExerciseCard';
 
 export const SelectExercise = ({ navigation, route }) => {
     const [showModalExercise, setShowModalExercise] = useState(false);
-    const [exercises, setExercises] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const [exercises, setExercises] = useState();
     const [nameExercise, setNameExercise] = useState();
     const [exerciseId, setExerciseId] = useState();
-    const muscleGroupId = route.params.muscleGroupId;
+    const [exerciseImage, setExerciseImage] = useState();
+    const mucleGroupId = route.params.mucleGroupId;
     const trainingName = route.params.trainingName;
 
-    function openModal(nameExercise, exerciseId) {
+    function openModal(nameExercise, exerciseId, exerciseGif) {
         setShowModalExercise(true);
         setNameExercise(nameExercise);
         setExerciseId(exerciseId);
+        setExerciseImage(exerciseGif);
     }
 
     const renderItem = ({ item }) => (
@@ -44,10 +45,8 @@ export const SelectExercise = ({ navigation, route }) => {
     }, []);
 
     async function GetExercises() {
-        setLoading(true);
-        const response = await GetExercisesByMuscleGroupId(muscleGroupId);
+        const response = await GetExercisesByMuscleGroupId(mucleGroupId);
         setExercises(response.data);
-        setLoading(false);
     }
 
     return (
@@ -64,6 +63,7 @@ export const SelectExercise = ({ navigation, route }) => {
                         />
                     }
                 />
+
                 <Title
                     FontSize={20}
                     marginTop={percentage(0.12, 'h')}
@@ -71,20 +71,17 @@ export const SelectExercise = ({ navigation, route }) => {
                 >
                     Exercícios
                 </Title>
-                <ListContainer heightContainer={'65%'} loading={loading}>
-                    {loading ? (
-                        <ActivityIndicator size="large" color="#FB6614" />
-                    ) : (
-                        <FlatList
-                            data={exercises}
-                            renderItem={renderItem}
-                            keyExtractor={item => item.exerciseId}
-                            contentContainerStyle={{
-                                gap: 10
-                            }}
-                        />
-                    )}
+                <ListContainer heightContainer={'65%'}>
+                    <FlatList
+                        data={exercises}
+                        renderItem={renderItem}
+                        keyExtractor={item => item.exerciseId}
+                        contentContainerStyle={{
+                            gap: 10
+                        }}
+                    />
                 </ListContainer>
+
                 <SelectedExerciseModal
                     visible={showModalExercise}
                     setShowModalExercise={setShowModalExercise}
